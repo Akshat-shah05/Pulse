d6c4f117b291ff9cba6f73e499ee677909f4d189
akshat-shah05 <akshatshah1605@gmail.com>
2024-05-23 22:03:06 -0400

    add friend functionality started

---
+++ src/components/addFriendButton/AddFriendButton.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Button } from '../ui/button'
+
+const AddFriendButton = () => {
+  return (
+    <Button variant="secondary" size="sm"> Add a Friend </Button>
+  )
+}
+
+export default AddFriendButton
