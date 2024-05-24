8ae94426689ead57ff67e62e84e48c3017708fdb
akshat-shah05 <akshatshah1605@gmail.com>
2024-05-23 22:03:33 -0400

    customs

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -11,17 +11,17 @@ const buttonVariants = cva(
       variant: {
         default: "bg-primary text-primary-foreground hover:bg-primary/90",
         destructive:
-          "bg-destructive text-destructive-foreground hover:bg-destructive/90 text-xl",
+          "bg-destructive text-destructive-foreground hover:bg-destructive/90 text-primary",
         outline:
           "border border-input bg-background hover:bg-accent hover:text-accent-foreground",
         secondary:
           "bg-secondary text-secondary-foreground hover:bg-secondary/80",
-        ghost: "hover:bg-accent hover:text-accent-foreground",
+        ghost: "hover:bg-accent hover:text-accent-foreground border",
         link: "text-primary underline-offset-4 hover:underline",
       },
       size: {
         default: "h-10 px-4 py-2",
-        sm: "h-9 rounded-md px-3",
+        sm: "h-9 rounded-md px-2",
         lg: "h-11 rounded-md px-8",
         xl: "h-20 w-30 rounded-md px-8",
         icon: "h-10 w-10",
