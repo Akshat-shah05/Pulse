27ca2bbceef539e5a4ca9e8d5a31aca7a6085e28
akshat-shah05 <akshatshah1605@gmail.com>
2024-07-28 15:54:01 -0400

    pushup page with server session

--- src/app/dashboard/exercises/pushups/page.tsx
+++ src/app/dashboard/exercises/pushups/page.tsx
@@ -1,8 +1,25 @@
 import PushUpPage from '@/components/exercises/pushup/PushupPage';
+import { getServerSession } from 'next-auth';
+import { db } from '@/lib/db';
 
-const page = () => {
+const page = async () => {
+    const user = await getServerSession();
+
+    // Get the email, check if email exists
+    let email = user?.user?.email;
+    console.log(email);
+
+    // if email exists, replace email with the associated username
+    if(email) {
+        const person = await db.user.findUnique({
+            where: {email: email}
+        })
+        email = person?.username;
+    }
+    console.log(email)
+
     return (
-        <PushUpPage />
+        <PushUpPage username={email}/>
     )
 }
 
