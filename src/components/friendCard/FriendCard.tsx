af2197bfeb4eca8849f096f81fe92cbad5082b4d
akshat-shah05 <akshatshah1605@gmail.com>
2024-07-28 15:55:27 -0400

    Made friend request card and significant style change in friendcard

--- src/components/friendCard/FriendCard.tsx
+++ src/components/friendCard/FriendCard.tsx
@@ -1,6 +1,7 @@
 'use-client'
 import React from 'react'
-import AddFriendButton from '../addFriendButton/AddFriendButton'
+import { Card } from '../ui/card'
+import { Avatar, AvatarFallback, AvatarImage } from '@radix-ui/react-avatar'
 
 interface friendProps {
     friend: string | undefined
@@ -9,16 +10,11 @@ interface friendProps {
 const FriendCard = ({friend}: friendProps) => {
   return (
     <>
-        <div
-        className="relative drop-shadow-xl w-1/2 h-10 overflow-hidden rounded-xl bg-black"
-        >
-            <div
-                className="absolute flex items-center justify-center text-white z-[1] opacity-90 rounded-xl inset-0.5 bg-[#323132]"
-            >
-                {friend}
+        <Card className="p-4 flex items-center gap-4 border-primary">
+            <div className="flex-1">
+                <div className="font-medium">{friend}</div>
             </div>
-            <div className="absolute w-56 h-48 bg-white blur-[50px] -left-1/2 -top-1/2"></div>
-        </div>
+        </Card>
     </>
   )
 }
