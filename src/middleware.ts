c02954bea997dedb8517a07dcdd997d3027581aa
akshat-shah05 <akshatshah1605@gmail.com>
2024-07-28 15:52:50 -0400

    added protected routes for exercise page

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,4 +1,4 @@
 export {default} from 'next-auth/middleware'
 
 // Add all protected routes over here
-export const config = {matcher: ['/dashboard']}
+export const config = {matcher: ['/dashboard', '/dashboard/exercises/pushups']}
