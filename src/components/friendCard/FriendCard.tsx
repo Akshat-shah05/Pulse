ceb6b1252d89bf9119c559788cb90a9ec18cc7fc
akshat-shah05 <akshatshah1605@gmail.com>
2024-07-28 17:12:22 -0400

    accept user state + added POST req to acccepting and declining for request card

--- src/components/friendCard/FriendCard.tsx
+++ src/components/friendCard/FriendCard.tsx
@@ -5,9 +5,10 @@ import { Avatar, AvatarFallback, AvatarImage } from '@radix-ui/react-avatar'
 
 interface friendProps {
     friend: string | undefined
+    username: string
 }
 
-const FriendCard = ({friend}: friendProps) => {
+const FriendCard = ({friend, username}: friendProps) => {
   return (
     <>
         <Card className="p-4 flex items-center gap-4 border-primary">
--- src/components/friendCard/FriendRequestCard.tsx
+++ src/components/friendCard/FriendRequestCard.tsx
@@ -1,26 +1,73 @@
 'use-client'
 import React from 'react'
 import { Button } from '../ui/button'
+import { Card } from '../ui/card'
+import { CheckIcon, XIcon } from '../icons/Icons'
 
 interface friendProps {
-    friend: string | undefined
+    friend: string | undefined,
+    username: string 
 }
 
-const FriendRequestCard = ({friend}: friendProps) => {
-  return (
-    <>
-        <div
-        className="relative drop-shadow-xl w-1/2 h-10 overflow-hidden rounded-xl bg-black flex flex-row"
-        >
-            <div
-                className="absolute flex items-center justify-center text-white z-[1] opacity-90 rounded-xl inset-0.5 bg-[#323132]"
-            >
-                {friend}
-            </div>
-            <div className="absolute w-56 h-48 bg-white blur-[50px] -left-1/2 -top-1/2"></div>
-        </div>
-    </>
-  )
+const FriendRequestCard = ({friend, username}: friendProps) => {
+
+    const handleAccept = async () => {
+        const response = await fetch("/api/friends/accept", {
+            method: "POST", 
+            headers: {
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+              friend: friend,
+              currentUser: username 
+            })
+        });
+      
+        if(response.ok) {
+            alert("Success!");
+            window.location.reload()
+        } else {
+            alert("Failure");
+        }
+    }
+
+    const handleDecline = async () => {
+        const response = await fetch("/api/friends/decline", {
+            method: "POST", 
+            headers: {
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+              friend: friend,
+              currentUser: username 
+            })
+        });
+      
+        if(response.ok) {
+            alert("Success!");
+            window.location.reload()
+        } else {
+            alert("Failure");
+        }
+    }
+
+    return (
+        <>
+            <Card className="p-4 flex items-center gap-4 border-primary">
+                <div className="flex flex-row items-center w-full">
+                    <div className="font-medium w-full">{friend}</div>
+                    <div className="w-full flex flex-row justify-end">
+                        <Button variant="rainbow" size="xs" className="px-6 ml-6 mr-1 flex-end" type='submit'>
+                            <div><CheckIcon onClick={handleAccept}/></div>
+                        </Button>
+                        <Button variant="rainbow" size="xs" className="px-6 ml-2 mr-1 flex-end" type='submit'>
+                            <div><XIcon onClick={handleDecline} /></div>               
+                        </Button>
+                    </div>
+                </div> 
+            </Card>
+        </>
+    )
 }
 
 export default FriendRequestCard
