f9768e129ec2e900759d53f9c8de27fc17b8c0e0
akshat-shah05 <akshatshah1605@gmail.com>
2024-08-06 21:42:55 -0400

    added multiplayer redirect option to main page

--- src/components/dashboard/Dashboard.tsx
+++ src/components/dashboard/Dashboard.tsx
@@ -2,6 +2,7 @@
 import Navbar from "../navbar/Navbar";
 import { useEffect, useState } from "react";
 import CardList from "../card/Cardlist";
+import { useRouter } from "next/navigation";
 
 // props for the username
 interface userProps {
@@ -11,6 +12,7 @@ interface userProps {
 // dashboard component, accepts username from top levle page.tsx
 const Dashboard = ({username}: userProps) => {
   // returns the Navbar alongside a welcome message. this is the entire dashboard page. LOTS TO COME!
+  const router = useRouter();
   const [loading, setLoading] = useState(true);
   useEffect(() => {
     let timer1 = setTimeout(() => setLoading(false), 2000)
@@ -18,6 +20,11 @@ const Dashboard = ({username}: userProps) => {
       clearTimeout(timer1)
     }
   }, [])
+
+  const handleMultiplayer = () => {
+    console.log('mult')
+    router.push("/dashboard/multiplayer")
+  }
 
   return (
     <>
@@ -36,11 +43,12 @@ const Dashboard = ({username}: userProps) => {
       ) : 
       (
         <>
-          <div className="flex min-h-screen w-full flex-col bg-background">
+          <div className="flex min-h-screen w-full flex-col bg-background items-center">
             <Navbar username={username}/>
             <h1 className="text-4xl flex flex-row justify-center mt-5"> Welcome {username}</h1>
             <h2 className="text-primary text-2xl flex flex-row justify-center mt-5"> Start by choosing an excercise</h2>
             <CardList />
+            <button className="flex flex-row justify-center w-4/5 p-16 bg-background border border-primary rounded-lg text-primary text-4xl" onClick={handleMultiplayer}> Multiplayer </button>
           </div>
         </>
       )
